dba0ed7fbbdf16c8837395eaf628d9b6fab58782
andyoung <1218853253@qq.com>
2023-04-06 13:09:08 +0800

    Update cccc.py

--- cccc.py
+++ cccc.py
@@ -10,7 +10,7 @@ from threading import Thread
 
 success_count = 0
 fail_count = 0
-total_count = 300
+total_count = 3000
 random.seed(time.time())
 proxy_list = []
 article_list = []
@@ -141,13 +141,13 @@ article_list.append("https://blog.csdn.net/agonie201218/article/details/12972475
 
 def do():
     global total_count
-    global count
+    global success_count
     while True:
         solve()
 
         if len(proxy_list) < 10:
             get_proxy_list()
-        if total_count < count:
+        if total_count < success_count:
             break
 
 
